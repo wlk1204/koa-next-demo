c6134798810294b1805fa6233def9cadd3c906b3
王立康 <wlk90god@gmail.com>
2020-07-23 09:49:35 +0800

    fix: tsconfig compile & tsc compile node file

--- app/router.ts
+++ app/router.ts
@@ -1,29 +1,29 @@
-import Router from 'koa-router'
+import Router from "koa-router";
 
 const router = (app) => {
-  const handle = app.getRequestHandler()
-  const router: any = new Router()
+  const handle = app.getRequestHandler();
+  const router: any = new Router();
 
-  router.get('/', async (ctx) => {
-    ctx.body = 'hello 🐷'
-  })
+  router.get("/", async (ctx) => {
+    ctx.body = "hello 🐷";
+  });
 
-  router.get('/a', async (ctx) => {
-    await app.render(ctx.req, ctx.res, '/a', ctx.query)
-    ctx.respond = false
-  })
+  router.get("/a", async (ctx) => {
+    await app.render(ctx.req, ctx.res, "/a", ctx.query);
+    ctx.respond = false;
+  });
 
-  router.get('/b', async (ctx) => {
-    await app.render(ctx.req, ctx.res, '/b', ctx.query)
-    ctx.respond = false
-  })
+  router.get("/b", async (ctx) => {
+    await app.render(ctx.req, ctx.res, "/b", ctx.query);
+    ctx.respond = false;
+  });
 
-  router.get('*', async (ctx) => {
-    await handle(ctx.req, ctx.res)
-    ctx.respond = false
-  })
+  router.get("*", async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
 
-  return router
-}
+  return router;
+};
 
-export default router
+export default router;
